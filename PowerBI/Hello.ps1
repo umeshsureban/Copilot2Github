7cb3338f0911d1d06615f98e0890d11d4b2cdcb2
umeshsureban <umesh_sv@hotmail.com>
2024-08-20 17:07:56 +0530

    Update and rename test.ps1 to Hello.ps1

---
+++ PowerBI/Hello.ps1
@@ -0,0 +1,8 @@
+# PowerShell script to display a message
+function Display-Message {
+    $message = "Hello Umesh, how are you doing?"
+    Write-Output $message
+}
+
+# Call the function
+Display-Message
