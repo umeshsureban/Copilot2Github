bd1d9c949ad854950c1c9742219d177ea66c22ff
umeshsureban <umesh_sv@hotmail.com>
2024-08-20 17:10:12 +0530

    Create Hello.ps1

---
+++ PowerBI/Hello.ps1
@@ -0,0 +1,8 @@
+# PowerShell script to display a message
+function Display-Message {
+    $message = "Hello Umesh, this script fan from MAinDuplicate branch?"
+    Write-Output $message
+}
+
+# Call the function
+Display-Message
